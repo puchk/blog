e2529a71995a89d2d24a3c809fe3b1cf7a14d404
puchk <kpuchalski@gmail.com>
2018-03-20 15:48:47 -0700

    connects app to express

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var express 		= require('express'),
-	  app					= express(),
 	  bodyParser 	=	require('body-parser'),
-	  mongoose 		=	require('mongoose');
+	  mongoose 		=	require('mongoose')
+	  app					= express(),
+	  PORT 				= process.env.PORT || 8080;
+
+mongoose.connect("mongodb://localhost/restful_blog");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(PORT, function() {
+	console.log("server is running on " +PORT);
+});
